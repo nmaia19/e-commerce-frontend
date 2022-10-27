492a53a47847606eee0d47f594ee9e17dde1416d
Nayane Maia <maia.nayane@gmail.com>
2022-10-27 16:55:33 -0300

    ajustes

--- scripts/home.js
+++ scripts/home.js
@@ -283,5 +283,46 @@ function getProdutos() {
 
 
 
+function displayFones() {
+    // pegando todos os produtos
+    let allProducts = getFones()
 
+    let gridLoja = document.getElementsByClassName('loja__content')[0]
+    //criando o novo elemento
+
+
+    allProducts.forEach(item => {
+        let divProduto = document.createElement('div')
+        divProduto.classList.add('content__item') // onde vai entrar o cartContentBoxHTML
+
+        let itemsDisplay = ``
+
+        itemsDisplay += `
+        <img src=${item.img} alt="" class="item__img" />
+        <h3 class="item__title">${item.nome}</h3>
+        <span class="item__price">${item.preco}</span>
+        <svg
+        class="add-cart"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        style="fill: rgb(255, 255, 255)"
+        >
+        <path
+            d="M21.822 7.431A1 1 0 0 0 21 7H7.333L6.179 4.23A1.994 1.994 0 0 0 4.333 3H2v2h2.333l4.744 11.385A1 1 0 0 0 10 17h8c.417 0 .79-.259.937-.648l3-8a1 1 0 0 0-.115-.921zM17.307 15h-6.64l-2.5-6h11.39l-2.25 6z"
+        ></path>
+        <circle cx="10.5" cy="19.5" r="1.5"></circle>
+        <circle cx="17.5" cy="19.5" r="1.5"></circle>
+        </svg>
+        `
+        divProduto.innerHTML = itemsDisplay
+        gridLoja.append(divProduto)
+    })
+}
+
+function getFones() {
+    let produtos = [...fones, ...tenis, ...roupas]
+    return produtos
+}
 
